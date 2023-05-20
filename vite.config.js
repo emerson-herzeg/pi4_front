170d4c6ab49fc1fc97fdc952fb21ec8d795e3d7d
Emerson Herzeg <emer.herzeg@gmail.com>
2023-05-19 22:55:26 -0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/api4',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicDir: '/api4' // Adicionando a propriedade publicDir
+  publicDir: '/api4', // Adicionando a propriedade publicDir
+  build: {
+    base: '/api4/' // Adicionando a propriedade base
+  }
 })
