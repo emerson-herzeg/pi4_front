5951fec6267510dbfa0013f6e3cbbe0929100cb6
Emerson Herzeg <emer.herzeg@gmail.com>
2023-05-20 11:43:03 -0300

    Ajustes dos ícones

--- src/views/ForecastView.vue
+++ src/views/ForecastView.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="background">
-    <div class="sun-container">
-      <img :src="sunIcon" alt="" class="responsive-image spin">
+    <div class="icon-container">
+      <img :src="currentIcon" alt="" class="responsive-image" :class="{ spin: currentIcon === sunIcon, moveHorizontal: currentIcon === rainIcon }">
     </div>
   </div>
 </template>
+
 <style>
 .responsive-image {
-  max-width: 50%; /* ajuste conforme necessário */
+  max-width: 75%; /* ajuste conforme necessário */
   height: auto;
 }
 
-.sun-container {
+.icon-container {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -30,14 +31,38 @@
 .spin {
   animation: spin 8s linear infinite;
 }
+
+@keyframes moveHorizontal {
+  0% { transform: translateX(-30%); }
+  50% { transform: translateX(30%); }
+  100% { transform: translateX(-30%); }
+}
+
+.moveHorizontal {
+  animation: moveHorizontal 5s ease-in-out infinite;
+}
 </style>
+
 <script>
 import sunIcon from '../assets/sun.png'
+import rainIcon from '../assets/rain.png'
 export default {
   data() {
     return {
-      sunIcon
+      sunIcon,
+      rainIcon,
+      currentIcon:this.getRandomIcon() // Altere isso para rainIcon para exibir a nuvem
     }
+  },
+  methods: {
+    getRandomIcon() {
+      // Este método seleciona aleatoriamente um dos ícones
+      return Math.random() < 0.5 ? this.sunIcon : this.rainIcon;
+    }
+  },
+  created() {
+    // Depois que o componente é criado, definimos currentIcon
+    this.currentIcon = this.getRandomIcon();
   }
 }
 </script>
